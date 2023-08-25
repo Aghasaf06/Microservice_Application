13ae158ca52eb4cb13429caa2d2874004420de0c
Aghasaf06 <agasfm90@gmail.com>
2023-08-25 08:30:03 +0400

    first commit

---
+++ src/main/java/microserviceapplication/model/Student.java
@@ -0,0 +1,21 @@
+package microserviceapplication.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "students")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Student {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private String surname;
+    private String groupName;
+}
